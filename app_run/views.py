e0784519a4ac16c5980c1d4105bd44ca3ca1b748
ivich <ivich82@gmail.com>
2026-05-20 22:28:09 +0300

    creat 'serializer.py' add 'RunSerializer' add 'RunViewSet' in 'views.py' create 'router' in 'urls.py'

--- app_run/views.py
+++ app_run/views.py
@@ -1,6 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.conf import settings
+from rest_framework import viewsets
+from .models import Run
+from .serializer import RunSerializer
+
+
 
 @api_view(['GET'])
 def company_details(request):
@@ -8,3 +13,7 @@ def company_details(request):
                'slogan': settings.SLOGAN,
                'contacts': settings.CONTACTS}
     return Response(details)
+
+class RunViewSet(viewsets.ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
