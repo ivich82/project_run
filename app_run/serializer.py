6254ea23e70c7bfd735f3bb6b2829eee2cc22a06
ivich <ivich82@gmail.com>
2026-05-23 23:08:34 +0300

    A new class, AthleteSerializer has been added to the serializer.py file. A new field, athlete_data, has been added to the serializer.py file of the RunSerializer class.  Optimized the RunViewSet class queryset in the view.py file

--- app_run/serializer.py
+++ app_run/serializer.py
@@ -2,10 +2,20 @@ from rest_framework import serializers
 from .models import Run
 from django.contrib.auth.models import User
 
+
+class AthleteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'last_name', 'first_name']
+
+
 class RunSerializer(serializers.ModelSerializer):
+    athlete_data = AthleteSerializer(source='athlete', read_only=True)
+
     class Meta:
         model = Run
         fields = '__all__'
+
 
 class UserSerializer(serializers.ModelSerializer):
     type = serializers.SerializerMethodField()
