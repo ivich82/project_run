f7ae632ba8f1cfcc556299550dd67bcff66c1256
ivich <ivich82@gmail.com>
2026-05-29 21:58:08 +0300

    Added the 'runs_finished' field to the UserSerializer class in the serializer.py file.Сorrected 2

--- app_run/serializer.py
+++ app_run/serializer.py
@@ -32,4 +32,4 @@ class UserSerializer(serializers.ModelSerializer):
     def get_runs_finished(self,obj):
 
         run_finished = Run.objects.filter(status='finished')
-        return f'{runs_finished.filter(athlete__id = obj.id).count()}'
+        return f'{run_finished.filter(athlete__id = obj.id).count()}'
