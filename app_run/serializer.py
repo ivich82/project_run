d062a0ad6d0fcc306dc4fe11ea70cc59e5b363bd
ivich <ivich82@gmail.com>
2026-05-29 23:30:13 +0300

    Added the 'runs_finished' field to the UserSerializer class in the serializer.py file.Cancel 9 task/

--- app_run/serializer.py
+++ app_run/serializer.py
@@ -19,11 +19,11 @@ class RunSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     type = serializers.SerializerMethodField()
-    # runs_finished = serializers.SerializerMethodField()
+    runs_finished = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ['id', 'date_joined', 'username', 'last_name', 'first_name', 'type']
+        fields = ['id', 'date_joined', 'username', 'last_name', 'first_name', 'type', 'runs_finished']
 
     def get_type(self, obj):
 
