7e4fb93d37f6d666241c2602919635fedfe03456
ivich <ivich82@gmail.com>
2026-06-09 18:21:58 +0300

    Fix condition in class PositionSerializer add validate_run,validate_latitude, validate_longitude in the file serializer.py

--- app_run/serializer.py
+++ app_run/serializer.py
@@ -64,16 +64,33 @@ class PositionSerializer(serializers.ModelSerializer):
         model = Position
         fields = '__all__'
 
-    def validate(self, data):
+    # def validate(self, data):
+    #     try:
+    #         run = Run.objects.get(id=data['run'])
+    #         if not (run.status == 'in_progress' and
+    #                 -90 <= data['latitude'] <= 90 and
+    #                 -180 <= data['longitude'] <= 180):
+    #             raise serializers.ValidationError('HTTP_400_BAD_REQUEST')
+    #     except Run.DoesNotExist:
+    #         raise serializers.ValidationError('HTTP_400_BAD_REQUEST')
+
+    def validate_run(self, value):
         try:
-            run = Run.objects.get(id=data['run'])
-            if not (run.status == 'in_progress' and
-                    -90 <= data['latitude'] <= 90 and
-                    -180 <= data['longitude'] <= 180):
+            run = Run.objects.get(id=value)
+            if not run.status == 'in_progress':
                 raise serializers.ValidationError('HTTP_400_BAD_REQUEST')
         except Run.DoesNotExist:
             raise serializers.ValidationError('HTTP_400_BAD_REQUEST')
+        return value
 
+    def validate_latitude(self, value):
+        if not -90 <= value <= 90:
+            raise serializers.ValidationError('HTTP_400_BAD_REQUEST')
+        return value
 
-        return  data
+    def validate_longitude(self, value):
+        if not -180 <= value <= 180:
+            raise serializers.ValidationError('HTTP_400_BAD_REQUEST')
+        return value
+
 
