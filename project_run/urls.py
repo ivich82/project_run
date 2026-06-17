9f7a2511acdeafbe2fe9032a01baa704ec46d2a0
ivich <ivich82@gmail.com>
2026-06-17 19:52:29 +0300

    Add in class StopAPIView new challenge in the file views.py

--- project_run/urls.py
+++ project_run/urls.py
@@ -18,14 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from app_run.views import company_details
 from rest_framework.routers import DefaultRouter
-from app_run.views import RunViewSet, UserViewSet, StartAPIView, StopAPIView, AthleteInfoAPIView, ChallegeViewSet, PositionViewSet
+from app_run.views import RunViewSet, UserViewSet, StartAPIView, StopAPIView, AthleteInfoAPIView, ChallengeViewSet, PositionViewSet
 
 
 
 router = DefaultRouter()
 router.register('api/runs', RunViewSet)
 router.register('api/users', UserViewSet)
-router.register('api/challenges',ChallegeViewSet)
+router.register('api/challenges',ChallengeViewSet)
 router.register('api/positions',PositionViewSet, basename='position')
 
 
