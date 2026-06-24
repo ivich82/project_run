45027ae96a9c7867440d4217de4a6b1aff671440
ivich <ivich82@gmail.com>
2026-06-24 14:44:03 +0300

    Add field items with connection manytomany in class CollectibleItem. Create migrate

--- app_run/models.py
+++ app_run/models.py
@@ -43,3 +43,4 @@ class CollectibleItem(models.Model):
     longitude = models.DecimalField(max_digits=8, decimal_places=4)
     picture = models.URLField()
     value = models.IntegerField()
+    items = models.ManyToManyField(User,  related_name='collectibleitems')
