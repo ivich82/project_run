d3713336e7990f11416c542664d8b1b11deb4dbb
ivich <ivich82@gmail.com>
2026-07-23 20:41:25 +0300

    Add model subscribe in the file admin.py

--- app_run/admin.py
+++ app_run/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from app_run.models import Run, AthleteInfo, Challenge, Position, CollectibleItem
+from app_run.models import Run, AthleteInfo, Challenge, Position, CollectibleItem, Subscribe
 
 admin.site.register(Run)
 admin.site.register(AthleteInfo)
 admin.site.register(Challenge)
 admin.site.register(Position)
 admin.site.register(CollectibleItem)
+admin.site.register(Subscribe)
