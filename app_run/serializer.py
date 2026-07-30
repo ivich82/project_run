45cd51b0ab4602589c56ce34d26181ce5bcfacb8
ivich <ivich82@gmail.com>
2026-07-30 20:14:32 +0300

    fix

--- app_run/serializer.py
+++ app_run/serializer.py
@@ -34,7 +34,7 @@ class UserSerializer(serializers.ModelSerializer):
         return 'coach' if obj.is_staff else 'athlete'
 
     def get_runs_finished(self,obj):
-        return obj.runs_finished
+        return obj.runs_finished if obj.runs_finished else None
 
 
 class AthleteDetailSerializer(UserSerializer):
