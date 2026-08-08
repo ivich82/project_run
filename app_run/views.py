802a17ceb2593cccfaab7d15ae47ef60c071dd8c
ivich <ivich82@gmail.com>
2026-08-08 21:55:46 +0300

    fix

--- app_run/views.py
+++ app_run/views.py
@@ -121,7 +121,7 @@ class StopAPIView(APIView):
 
             annotated_queryset = User.objects.filter(is_superuser=False).annotate(
                 runs_finished=Count('run', filter=Q(run__status='finished')),
-                rating=Avg('coach__rating'))
+                rating=Avg('coach__rating')
             )
             athlete = get_object_or_404(annotated_queryset, id=run.athlete.id)
             serializer = UserSerializer(athlete)
